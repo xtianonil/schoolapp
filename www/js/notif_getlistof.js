53eade7d8f374dadc8699f839380553e172ca562
xtianonil <co.tolentino@gmail.com>
2016-12-01 10:28:32 +0800

    #

--- www/js/notif_getlistof.js
+++ www/js/notif_getlistof.js
@@ -3,6 +3,36 @@ document.addEventListener("deviceready", onDeviceReady, false);
 function onDeviceReady() {
     $(document).delegate("#home", "pageshow", function() {
         //console.log("Hello world!");
+        $.post(localStorage.webhost+"notif_getlistof.php",{userid:localStorage.user_id})
+			.done(function(result){
+				var group_membership = JSON.parse(result);
+				$("#notifs_list").empty();
+				for (var i=0; i<group_membership.length; i++)
+				{	//for every group the user is a member of 
+					//fetch notifs
+					var ul = $("<ul data-role='listview' data-theme='a'>");
+					//$("#notifs_list").append(ul);
+					$.post(localStorage.webhost+"notif_view.php",{groupid:group_membership[i].group_id})
+						.done(function(data){
+							var notifs = JSON.parse(data);
+							for (var i=0; i<notifs.length; i++)
+							{
+								var li = $("<li><a href='#' style='text-decoration:none'>message: "	+notifs[i].payload+"</a></li>");
+								ul.append(li);
+								var li = $("<li><a href='#' style='text-decoration:none'>posted by: "	+notifs[i].username+"</a></li>");
+								ul.append(li);
+								var li = $("<li><a href='#' style='text-decoration:none'>posted on: "	+notifs[i].created_on+"</a></li>");
+								ul.append(li);
+								ul.append("<hr>");
+							}
+						});
+					ul_close = $("</ul>");
+					ul.append(ul_close);
+					$("#notifs_list").append(ul);
+					//$("#notifs_list").append("</ul>");
+				}
+				//$("#notifs_list").append("</ul>");
+			});
     });
 }
 
