d116ca8b0249bdfa89f228d13e0c77ba567fb6c3
xtianonil <co.tolentino@gmail.com>
2017-01-13 14:18:39 +0800

    #

--- www/js/footer.js
+++ www/js/footer.js
@@ -3,7 +3,7 @@ var logout = $("<p><a href='#' id='logout'> logout </a></p>");
 //logout.prepend("signed in as: "+localStorage.user_id);
 $("#footer_div").append(logout);
 $("#logout").click(function(){
-	window.location.href = "index.html#not_logged_in";
+	//window.location.href = "index.html#not_logged_in";
 	location.reload();
 	$("#login").html('Login');
 	localStorage.clear();
