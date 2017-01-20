03939cc07a67902fc0d02f81863d10475c85fd7f
xtianonil <co.tolentino@gmail.com>
2017-01-20 14:58:35 +0800

    #

--- www/js/notifs_feed.js
+++ www/js/notifs_feed.js
@@ -20,13 +20,13 @@
     		.done(function(data){
     			//alert(data);
     			var notif = JSON.parse(data);
-				adjusted_date = moment(notif[0].created_on).add(1,'days').utcOffset('-0200').format('h:mm:ss a');
+				adjusted_date = moment(notif[0].created_on).add(1,'days').utcOffset('-0200').format('h:mm a');
 
 				if ( notif[0].payload.length < 25 )
 					var short_payload = notif[0].payload;
 				else
 					var short_payload = (notif[0].payload).substring(0,25)+"...";
-				$("#notifs_list").prepend($("<li class='notif_item unread' data-icon='false' id="+notif[0].notif_id+"><a><div style='font-weight:900'>"+notif[0].lname+" "+notif[0].fname+"</div><div style='float:right;'>"+adjusted_date+"</div><br><div style='font-weight:900'>"+short_payload+"</div></a></li>"));
+				$("#notifs_list").prepend($("<li class='notif_item unread' data-icon='false' id="+notif[0].notif_id+"><a><div style='font-weight:900'>"+notif[0].lname+" "+notif[0].fname+"<div style='float:right;'>"+adjusted_date+"</div></div><br><div style='font-weight:900'>"+short_payload+"</div></a></li>"));
     			//alert("pasok dito");
     			$("#notifs_list").listview("refresh");
 
@@ -39,7 +39,7 @@
 				    		$("#notif_details_list").empty();
 				    		//$("#notif_details_list").append( $("<ul data-role='listview'></ul>") ); 
 				    		$("#notif_details_list").append( $("<li>From: &nbsp;&nbsp;&nbsp;"+notif[0].lname+" "+notif[0].fname+"</li>") );
-				    		$("#notif_details_list").append( $("<li>Posted: "+moment(notif[0].created_on).add(1,'days').utcOffset('-0200').format('h:mm:ss a MMMM D')+"</li>") );
+				    		$("#notif_details_list").append( $("<li>Posted: "+moment(notif[0].created_on).add(1,'days').utcOffset('-0200').format('h:mm:ss a, MMMM D, YYYY')+"</li>") );
 				    		$("#notif_details_list").append( $("<br><ul><li>"+notif[0].payload+"</li></ul>") );
 				    		$("#notif_details_list").listview("refresh");
 				    	});
@@ -54,22 +54,15 @@
 				    //showNotifs();
 					//setTimeout(function(){$("#notif_details_popup").popup("open",'positionTo: window');},100);
 				}//end of tapHandler function
-    			//on taphold, show notification options
-				$( ".notif_item" ).on( "taphold", tapholdHandler );
-				function tapholdHandler( event ){
+    			//on swipeleft, show notification options
+				$( ".notif_item" ).on( "swipeleft", swipeleftHandler );
+				function swipeleftHandler( event ){
 				    localStorage.notifid_selected = $(this).attr('id');
 					if ( $(this).hasClass('unread') )
 						$("#notif_toggleread").text("Mark as read");
 					else if ( $(this).hasClass('read') )
 						$("#notif_toggleread").text("Mark as unread");
 					setTimeout(function(){$("#notif_details_popup_options").popup("open");},100);
-				}
-				//on swipeleft, show notification options
-				$( ".notif_item" ).on( "swipeleft", swipeleftHandler );
-				function swipeleftHandler( event ){
-				    localStorage.notifid_selected = $(this).attr('id');
-				    $(this).empty();
-				    $(this).append( $("<li>A</li>") );
 				}
     		});
     	//$("#notifs_list").prepend($("<li class='notif_item unread' data-icon='false' id="+group_id+">"+group_id+"</li>"));
@@ -101,7 +94,7 @@ function showNotifs()
 
 				var date = moment(field.created_on).add(1,'days').utcOffset('-0200');
 				if ( moment().diff(field.created_on,'days') < 1 )
-					adjusted_date = date.format('h:mm:ss a');
+					adjusted_date = date.format('h:mm a');
 				else if ( moment().diff(field.created_on,'days') >=1 && moment().diff(field.created_on,'days') < 2 )
 					adjusted_date = date.calendar();
 				else if ( moment().diff(field.created_on,'days') < 7 )
@@ -127,7 +120,7 @@ function showNotifs()
 					$("#notifs_list").append($("<li class='notif_item unread' data-icon='false' id="+field.notif_id+"><a><div style='font-weight:900'>"+field.lname+" "+field.fname+"<div style='float:right;'>"+adjusted_date+"</div></div><br><div style='font-weight:900'>"+short_payload+"</div></a></li>"));
 					//$("#notifs_list").append( $("<li data-icon='true'><a href='#' style='font-weight:900;' class='notif_item unread' id="+field.notif_id+">"+short_payload+"</a></li>") );		
 				else if (field.status_onfeed === 'read')
-					$("#notifs_list").append($("<li class='notif_item read' data-icon='false' id="+field.notif_id+"><a><div style='font-weight:100;'>"+field.lname+" "+field.fname+"<div style='float:right;'>"+adjusted_date+"</div></div><br><div style='font-weight:100'>"+short_payload+"</div></a></li>"));
+					$("#notifs_list").append($("<li class='notif_item read' data-icon='false' id="+field.notif_id+"><a style='background-color:#E8E8E8'><div style='font-weight:100;'>"+field.lname+" "+field.fname+"<div style='float:right;'>"+adjusted_date+"</div></div><br><div style='font-weight:100'>"+short_payload+"</div></a></li>"));
 					//$("#notifs_list").append( $("<li data-icon='true'><a href='#' style='font-weight:100;' class='notif_item read' id="+field.notif_id+">"+short_payload+"</a></li>") );		
 			});
 			$("#notifs_list").listview("refresh");
@@ -141,7 +134,7 @@ function showNotifs()
 				    		$("#notif_details_list").empty();
 				    		//$("#notif_details_list").append( $("<ul data-role='listview'></ul>") ); 
 				    		$("#notif_details_list").append( $("<li>From: &nbsp;&nbsp;&nbsp;"+notif[0].lname+" "+notif[0].fname+"</li>") );
-				    		$("#notif_details_list").append( $("<li>Posted: "+moment(notif[0].created_on).add(1,'days').utcOffset('-0200').format('h:mm:ss a MMMM D')+"</li>") );
+				    		$("#notif_details_list").append( $("<li>Posted: "+moment(notif[0].created_on).add(1,'days').utcOffset('-0200').format('h:mm:ss a, MMMM D, YYYY')+"</li>") );
 				    		$("#notif_details_list").append( $("<br><ul><li>"+notif[0].payload+"</li></ul>") );
 				    		$("#notif_details_list").listview("refresh");
 				    	});
@@ -157,30 +150,23 @@ function showNotifs()
 					//setTimeout(function(){$("#notif_details_popup").popup("open",'positionTo: window');},100);
 				}//end of tapHandler function
 
-				//on taphold, show notification options
-				$( ".notif_item" ).on( "taphold", tapholdHandler );
-				function tapholdHandler( event ){
-				    localStorage.notifid_selected = $(this).attr('id');
-					if ( $(this).hasClass('unread') )
-						$("#notif_toggleread").text("Mark as read");
-					else if ( $(this).hasClass('read') )
-						$("#notif_toggleread").text("Mark as unread");
-					setTimeout(function(){$("#notif_details_popup_options").popup("open");},100);
-				}
 				//on swipeleft, show notification options
 				$( ".notif_item" ).on( "swipeleft", swipeleftHandler );
 				function swipeleftHandler( event ){
 				    localStorage.notifid_selected = $(this).attr('id');
-				    $(this).empty();
-				    $(this).append( $("<li>A</li>") );
-				    /*
 					if ( $(this).hasClass('unread') )
 						$("#notif_toggleread").text("Mark as read");
 					else if ( $(this).hasClass('read') )
 						$("#notif_toggleread").text("Mark as unread");
-					setTimeout(function(){$("#notif_details_popup_options").popup("open");},100);
-					*/
-					//event.preventDefault();
+
+					setTimeout(function(){
+						//$(this).attr('id')
+						//alert(localStorage.notifid_selected);
+						$("#notif_details_popup_options").popup("open",{positionTo: '#'+localStorage.notifid_selected});
+						
+						//setTimeout(function(){},100);
+						//$("#notif_details_popup_options").popup("reposition", {positionTo: 'origin' });
+					},100);
 				}
 				$("#notif_toggleread").click(function(){
 					if ( $(this).text() === "Mark as read" )
@@ -207,111 +193,7 @@ function showNotifs()
 		});
 	//setTimeout(showNotifs,60000);	//refreshes every 5m...1minute = 60000ms
 }
-function showNotifsFeed2()
-{
-	$.post(localStorage.webhost+"group_listall2.php",{userid:localStorage.user_id})
-		.done(function(result){
-			var groups = JSON.parse(result);
-			$("#notifs_list").empty();
-			$.each(groups, function(i, field)
-			{	//for every group the user is a member of 
-				//fetch notifs
-				//var ul = $("<ul data-role='listview' data-theme='a'>");
-				//$("#notifs_list").empty();
-				$.post(localStorage.webhost+"notif_listuserspecific.php",{groupid:field.group_id,userid:localStorage.user_id})
-					.done(function(data){
-						var notifs = JSON.parse(data);
-						$.each(notifs, function(i, field)
-						{
-							if (field.status_onfeed === 'unread')
-								$("#notifs_list").append( $("<li data-icon='true'><a href='#' style='font-weight:900;' class='notif_item unread' id="+field.notif_id+">"+field.payload+"</a></li>") );		
-								//$("#notifs_list").append( $("<li data-icon='true'><a href='#' style='font-weight:900;' class='notif_item unread' id="+field.notif_id+">"+field.payload+" </font>("+field.notif_id+")<br>posted by: "+field.lname+", "+field.fname+" (<font color='red'>"+field.created_on+"</font>)"+"</a></li>") );		
-							else if (field.status_onfeed === 'read')
-								$("#notifs_list").append( $("<li data-icon='true'><a href='#' style='font-weight:100;' class='notif_item read' id="+field.notif_id+">"+field.payload+"</a></li>") );		
-								//$("#notifs_list").append( $("<li data-icon='true'><a href='#' style='font-weight:100;' class='notif_item read' id="+field.notif_id+">"+field.payload+" ("+field.notif_id+")<br>posted by: "+field.lname+", "+field.fname+" (<font color='red'>"+field.created_on+"</font>)"+"</a></li>") );		
-						});
-						$("#notifs_list").listview("refresh");
 
-						/*
-						$(".notif_item").click(function(){
-							//alert($(this).attr('id'));
-							localStorage.notifid_selected = $(this).attr('id');
-							if ( $(this).hasClass('unread') )
-								$("#notif_toggleread").text("Mark as read");
-							else if ( $(this).hasClass('read') )
-								$("#notif_toggleread").text("Mark as unread");
-							setTimeout(function(){$("#notif_details_popup").popup("open");},100);
-							});*/
-						
-						//on tap, show full notification details
-						$( ".notif_item" ).on( "tap", tapHandler );
-						function tapHandler( event ){
-							//alert("swiped");
-						    //$( event.target ).addClass( "swipe" );
-						    localStorage.notifid_selected = $(this).attr('id');
-						    //$("#notif_details_list").empty();
-						    $.post(localStorage.webhost+"notif_listspecific.php",{notifid:localStorage.notifid_selected})
-						    	.done(function(data){
-						    		//alert(data);
-						    		var notif = JSON.parse(data);
-						    		$("#notif_details_list").empty();
-						    		$("#notif_details_list").append( $("<li>From: "+notif[0].lname+", "+notif[0].fname+"</li>") );
-						    		$("#notif_details_list").append( $("<li>Posted:"+notif[0].created_on+"</li>") );
-						    		$("#notif_details_list").append( $("<li>"+notif[0].payload+"</li>") );
-						    		$("#notif_details_list").listview("refresh");
-						    	});
-						    $.mobile.changePage("index.html#notif_view", {
-							        transition: "slide",
-							        reverse: false	//from right
-							    });
-							//setTimeout(function(){$("#notif_details_popup").popup("open",'positionTo: window');},100);
-						}//end of tapHandler function
-
-						//on taphold, show notification options
-						$( ".XXXX" ).on( "taphold", tapholdHandler );
-						function tapholdHandler( event ){
-							//alert("swiped");
-						    //$( event.target ).addClass( "swipe" );
-						    localStorage.notifid_selected = $(this).attr('id');
-							if ( $(this).hasClass('unread') )
-								$("#notif_toggleread").text("Mark as read");
-							else if ( $(this).hasClass('read') )
-								$("#notif_toggleread").text("Mark as unread");
-							setTimeout(function(){$("#notif_details_popup_options").popup("open");},100);
-						}
-						$("#notif_toggleread").click(function(){
-							//alert($(this).text());
-							if ( $(this).text() === "Mark as read" )
-								var toggle_option = "unread";
-							else if ( $(this).text() === "Mark as unread" )
-								var toggle_option = "read";
-							$.post(localStorage.webhost+"notif_toggleread.php",{userid:localStorage.user_id,notifid:localStorage.notifid_selected,toggleoption:toggle_option})
-								.done(function(toggleread_success){
-									if (toggleread_success)
-									{
-										//alert("Notif marked successfully.");
-										location.reload();
-									}
-								});
-							});//end of notif_toggleread
-						$("#notif_removefromfeed").click(function(){
-							$.post(localStorage.webhost+"notif_removefromfeed.php",{userid:localStorage.user_id,notifid:localStorage.notifid_selected})
-								.done(function(removedfromfeed){
-									//alert("Notif removed from feed.");
-									location.reload();
-								});
-							});//end of notif_removefromfeed
-						/*
-						$( ".notif_item" ).on( "swipe", swipeHandler );
-						function swipeHandler( event ){
-							alert("swiped");
-						    $( event.target ).addClass( "swipe" );
-						  }*/
-					});
-			});//end of each
-		});
-	setTimeout(showNotifsFeed2,60000);	//refreshes every 5m...1minute = 60000ms
-}
 $("#notif_back_btn").click(function(){
 	$.post(localStorage.webhost+"notif_toggleread.php",{userid:localStorage.user_id,notifid:localStorage.notifid_selected,toggleoption:"unread"})
     	.done(function(){
