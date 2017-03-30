1f2bda6238213c141d0b1ba275d580987a2e6452
player00326 <co.tolentino@gmail.com>
2017-03-30 10:31:15 +0800

    #

--- www/js/index.js
+++ www/js/index.js
@@ -79,7 +79,7 @@ var app = {
                 data.title,           // title
                 'Ok'                  // buttonName
             );
-            alert("context: " + data.largeIcon); //this works
+            alert("context: " + data.cntxt); //this works
        });//end of push.on('notif')
     }//end of setupPush: function()
 };
