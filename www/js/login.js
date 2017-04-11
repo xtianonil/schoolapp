fcd94fe4591b46e4f059bbc732edf7a4ff1f2500
player00326 <co.tolentino@gmail.com>
2017-04-11 17:42:20 +0800

    #

--- www/js/login.js
+++ www/js/login.js
@@ -95,6 +95,7 @@ function deviceLogin()
 {
 	$.post(localStorage.webhost+"device_isregistered.php",{regid:localStorage.reg_id})
 		.done(function(data){
+			alert(data);
 			if (data === "is_registered")
 			{	//means user has logged in on this device before, just update device details
 				$.post(localStorage.webhost+"device_update.php",
